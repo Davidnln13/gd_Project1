ec37b812b06395cf8d0eca7dcc68bdc739311442
Davidnln13 <davidnln13@yahoo.com>
2017-11-21 18:25:03 +0000

    added main for test

---
+++ main.js
@@ -0,0 +1,13 @@
+var gameNamespace = {};
+/**
+* Main
+* @desc just defines a new game and initialises
+*/
+function main()
+{
+  //new game
+  const g = new Game();
+  gameNamespace.game = g;
+  g.initWorld();
+
+}
