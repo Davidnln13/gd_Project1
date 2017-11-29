c40f9407abc47cec9a73890c06a29c3f1624d5a3
Davidnln13 <davidnln13@yahoo.com>
2017-11-29 17:49:52 +0000

    Scrolling background implemeneted

--- Game.js
+++ Game.js
@@ -1,24 +1,52 @@
 //reviewed
 class Game
 {
+
   constructor()
   {
-    this.canvas = null;
+
+    gameNamespace.canvas = null;
   }
-  /**
+    /**
   * initWorld
   * @desc Initialises game world
   */
   initWorld()
   {
-    //context
-    this.ctx;
-    //initialise canvas
-    this.initCanvas();
 
-    this.canvas.addEventListener("touchstart", this.onTouchStart.bind(this));
-    console.log("Initialising Game World");
+    gameNamespace.posOne = 0;
+    gameNamespace.posTwo = -880;
+    //backgrounds
+    gameNamespace.backgroundOneDiv;
+    gameNamespace.backgroundTwoDiv;
+    //enum
+    gameNamespace.GamestateEnum = {
+                                    MAIN:0,
+                                    GAME:1,
+                                    OPTIONS:2,
+                                    HIGHSCORE:3,
+                                    EXIT:4
+                                  }
+    gameNamespace.gamestate = gameNamespace.GamestateEnum.MAIN;
+    gameNamespace.mouseX = -100;
+    gameNamespace.mouseY = -100
+
+    //different screens
+    gameNamespace.mainMenu = new MainMenu();
+    gameNamespace.gameMenu = new GameMenu();
+    gameNamespace.optionsMenu = new OptionsMenu();
+    gameNamespace.highscoreMenu = new HighscoreMenu();
+    gameNamespace.exitMenu = new ExitMenu();
+
+    //context
+    gameNamespace.ctx
+    //initialise canvas
+    this.initCanvas()
+    gameNamespace.canvas.addEventListener("touchstart", this.onTouchStart.bind(this))
+    console.log("Initialising Game World")
     this.background();
+
+    gameNamespace.game.update();
     //this.ctx.addEventListener("touchmove", this.onTouchMove.bind(this));
   //  this.ctx.addEventListener("touchend", onTouchEnd);
     window.addEventListener("keydown", function(e)
@@ -38,8 +66,42 @@ class Game
  */
  update()
  {
-   gameNamespace.game.draw();
+   console.log(gameNamespace.posOne, gameNamespace.posTwo);
+   if(gameNamespace.posOne > 880)
+   {
+     gameNamespace.posOne = 0-880;
+   }
+   if(gameNamespace.posTwo > 880)
+   {
+     gameNamespace.posTwo = -880;
+   }
+   gameNamespace.posOne++;
+   gameNamespace.backgroundOneDiv.style.top = gameNamespace.posOne + 'px';
 
+   gameNamespace.posTwo++;
+   gameNamespace.backgroundTwoDiv.style.top = gameNamespace.posTwo +'px';
+   gameNamespace.game.draw();
+   //update menus
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.MAIN)
+   {
+       gameNamespace.mainMenu.update();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.GAME)
+   {
+       gameNamespace.gameMenu.update();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.OPTIONS)
+   {
+       gameNamespace.optionsMenu.update();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.HIGHSCORE)
+   {
+       gameNamespace.highscoreMenu.update();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.EXIT)
+   {
+       gameNamespace.exitMenu.update();
+   }
    //recursively calls update of game : this method
    window.requestAnimationFrame(gameNamespace.game.update);
  }
@@ -47,9 +109,36 @@ class Game
  * draw
  * @desc draws the sprites
  */
+
  draw()
  {
 
+   gameNamespace.ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+   // gameNamespace.ctx.fillStyle = '#FF0000';
+   // gameNamespace.ctx.fillRect(gameNamespace.mouseX,gameNamespace.mouseY,5,5);
+   // console.log(gameNamespace.mouseX,gameNamespace.mouseY);
+
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.MAIN)
+   {
+     gameNamespace.mainMenu.draw();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.GAME)
+   {
+     gameNamespace.gameMenu.draw();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.OPTIONS)
+   {
+     gameNamespace.optionsMenu.draw();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.HIGHSCORE)
+   {
+     gameNamespace.highscoreMenu.draw();
+   }
+   if(gameNamespace.gamestate === gameNamespace.GamestateEnum.EXIT)
+   {
+     gameNamespace.exitMenu.draw();
+   }
  }
  /**
 * initCanvas
@@ -57,23 +146,29 @@ class Game
 */
 initCanvas()
 {
-  this.canvas = document.createElement("canvas");
-  this.canvas.id = "canvas";
-  this.canvas.width = window.innerwidth;
-  this.canvas.height = window.innerheight;
-  document.body.appendChild(this.canvas);
-  this.ctx = this.canvas.getContext("2d");
-
+  gameNamespace.canvas = document.createElement("canvas");
+  gameNamespace.canvas.id = "canvas";
+  gameNamespace.canvas.width = window.innerWidth;
+  gameNamespace.canvas.height = window.innerHeight;
+  document.body.appendChild(gameNamespace.canvas);
+  gameNamespace.ctx = gameNamespace.canvas.getContext("2d");
 
 }
 background()
 {
-  var div = document.createElement("div");
-  div.innerHTML='<img src=\'/Resources/Images/background.png\'>'
-  div.style.left=0;
-  div.style.top = 0;
-  div.style.position = "absolute";
-  document.body.appendChild(div);
+  gameNamespace.backgroundOneDiv = document.createElement("div");
+  gameNamespace.backgroundOneDiv.innerHTML='<img src=./Resources/Images/background.png>';
+  gameNamespace.backgroundOneDiv.style.left=0;
+  gameNamespace.backgroundOneDiv.style.top = 0;
+  gameNamespace.backgroundOneDiv.style.position = "absolute";
+  document.body.appendChild(gameNamespace.backgroundOneDiv);
+
+  gameNamespace.backgroundTwoDiv = document.createElement("div");
+  gameNamespace.backgroundTwoDiv.innerHTML='<img src=./Resources/Images/background.png>';
+  gameNamespace.backgroundTwoDiv.style.left=0;
+  gameNamespace.backgroundTwoDiv.style.top = 0  ;
+  gameNamespace.backgroundTwoDiv.style.position = "absolute";
+  document.body.appendChild(gameNamespace.backgroundTwoDiv);
 }
 /**
 * ontouchstart
@@ -82,6 +177,9 @@ background()
 onTouchStart(e)
 {
     e.preventDefault();
+    var touches = e.touches;
+    gameNamespace.mouseX = touches[0].clientX;
+    gameNamespace.mouseY = touches[0].clientY;
 }
 /**
 * onTouchMove
---
+++ GameMenu.js
@@ -0,0 +1,25 @@
+class GameMenu
+{
+  constructor()
+  {
+
+  }
+  update()
+  {
+    //main
+    if(gameNamespace.mouseX > 172 && gameNamespace.mouseY > 558 && gameNamespace.mouseX < 288 && gameNamespace.mouseY < 602)
+    {
+      gameNamespace.gamestate = gameNamespace.GamestateEnum.MAIN;
+    }
+  }
+  draw()
+  {
+    gameNamespace.ctx.font = '48px impact';
+    gameNamespace.ctx.fillStyle = '#FF0000';
+    gameNamespace.ctx.fillText('GAME',window.innerWidth/2-60,100);
+
+    gameNamespace.ctx.font = '32 impact';
+    gameNamespace.ctx.fillText('BACK',window.innerWidth/2-70,600);
+
+  }
+}
